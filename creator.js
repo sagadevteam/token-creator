bf51a553d9b8c2a6e1974b852e61309a71d74386
Ben Lu <coopermilk123@gmail.com>
2018-08-23 16:09:48 -0700

    Add confirmation detection

--- creator.js
+++ creator.js
@@ -3,6 +3,7 @@ const env = require('./env')
 const mysql = require('mysql')
 const Web3 = require('web3')
 var sleep = require('sleep')
+let Guard = require('web3-guard')
 
 const web3 = new Web3(new Web3.providers.HttpProvider(env.web3Url))
 const HotelCalABI = HotelCal.abi
@@ -19,9 +20,10 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-let targetTicketsSQL = `SELECT t.*, u.eth_addr
+let targetTicketsSQL = `SELECT t.*, u.eth_addr, i.price
                         FROM tickets as t
                         LEFT JOIN users as u on (t.user_id = u.user_id)
+                        INNER JOIN inventories as i on (i.inventory_id = t.inventory_id)
                         WHERE t.on_chain = '0'`
 
 let updateOnChainSQL = `UPDATE tickets
@@ -54,16 +56,38 @@ let updateToOnChain = async (ticketID) => {
   }
 }
 
+let guard = new Guard(web3)
+let confirmations = 6
+guard = guard.bind('1122').confirm(confirmations)
+guard.on(hotel, hotel.Transfer({
+  _from: '0x0000000000000000000000000000000000000000',
+  _to: web3.eth.coinbase
+}).watch(async (err, event) => {
+  if (err) {
+    console.error(err)
+  } else {
+    if (!event.confirmed) {
+      console.log('Not confirm mint tranasction: ' + event.transactionHash)
+      guard.wait(event)
+    } else {
+      console.log('Confirm mint tranasction: ' + event.transactionHash)
+      let tokenID = parseInt(event.args._tokenId)
+      await updateToOnChain(tokenID)
+      console.log('Update token status to on-chain: ' + tokenID)
+    }
+  }
+}))
+
 let processTickets = async () => {
   let tickets = await getOffChainTickets()
   for (let i = 0; i < tickets.length; i++) {
     console.log('Found tickets: ' + tickets.length)
     let ticket = tickets[i]
     try {
-      let txHash = await hotel.mint(web3.eth.coinbase, ticket.ticket_id, 'http://google.com',
+      let txHash = await hotel.mint(web3.eth.coinbase, ticket.ticket_id, 'http://google.com', ticket.price,
         { from: web3.eth.coinbase, to: hotel.address, gas: 470000 })
       console.log(txHash)
-      await updateToOnChain(ticket.ticket_id)
+      guard.listen(txHash)
     } catch (e) {
       console.log(e)
     }
@@ -71,11 +95,12 @@ let processTickets = async () => {
 }
 
 let main = async () => {
-  while (true) {
+  console.log('Wait for uploading tickets...')
+  await processTickets()
+  setInterval(async () => {
     console.log('Wait for uploading tickets...')
     await processTickets()
-    sleep.sleep(60)
-  }
+  }, 86400)
 }
 
 main()
